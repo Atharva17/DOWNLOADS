681b1d679856dca2cbbc2ae6f7c1a7122a078b9b
Harshini A <harshinidurai98@gmail.com>
2017-09-01 06:51:11 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+char char;
+scanf("%c",&char);
+if(char=='a' || char=='e' || char=='i' || char=='o' || char=='u')
+printf("the given character is vowel")
+else
+printf("the given character is consonent");
+}
