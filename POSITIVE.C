8f4b1473e0712bebc8f56567f2c47ae94d6713eb
Harshini A <harshinidurai98@gmail.com>
2017-09-01 06:35:08 +0530

    Update POSITIVE.C

--- POSITIVE.C
+++ POSITIVE.C
@@ -5,8 +5,8 @@ int a;
 scanf("%d",&a);
 if(a>0) 
 printf("the number %d is positive",a);
-else if(a<0)
-printf("the number %d is negative",a);
+else if(a=0)
+printf("the number %d is zero ",a);
 else
-printf("the number %d is zero",a);
+printf("the number %d is negative",a);
 }
