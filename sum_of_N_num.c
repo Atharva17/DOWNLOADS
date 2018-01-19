809c25590385d78dbdfd06fc07926486d399f543
Harshini A <harshinidurai98@gmail.com>
2018-01-19 16:03:31 +0530

    Update sum_of_N_num.c

--- sum_of_N_num.c
+++ sum_of_N_num.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
 int main()
 {
-int n,i,sum=0;
+int n,i,sum=0,k,N[100];
 scanf("%d",&n);
+scanf("%d",&k);
 for(i=0;i<n;i++)
-sum=sum+i;
+ scanf("%d",&N[i]);
+for(i=0;i<k;i++)
+sum=sum+N[i];
 printf("sum=%d",sum);
 }
